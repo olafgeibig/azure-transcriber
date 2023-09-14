e66e53357e17b003e72ea4bff026555ff21bc498
Olaf Geibig <GEO5BE4@bosch.com>
2023-09-14 16:35:15 +0200

    aider: Moved main function from transcriber.py to app.py
    # Aider chat conversation:
    
    USER: move main to `app.py`
    
    ASSISTANT: To move the `main` function from `transcriber.py` to `app.py`, we will follow these steps:
    
    1. Remove the `main` function from `transcriber.py`.
    2. Add the `main` function to `app.py`.
    
    Let's start with removing the `main` function from `transcriber.py`.
    
    ```python
    transcriber.py
    <<<<<<< HEAD
    def main():
        st.title("Azure Meting Transcription")
        uploaded_file = st.file_uploader("Choose a file", type=["mp3"])
        if uploaded_file is not None:
            file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type,"FileSize":uploaded_file.size}
            st.write(file_details)
    
            # Convert the uploaded mp3 file to a wav file with a spinner to indicate progress
            wav_file_path = "temp.wav"
            with st.spinner('Converting audio file to wav format...'):
                convert_mp3_to_wav(uploaded_file, wav_file_path)
    
            with st.spinner('Processing audio file...'):
                transcriptions = conversation_transcription(wav_file_path)
            st.text_area("Transcription", "\n".join(transcriptions), height=200)
    
    if __name__ == "__main__":
        main()
    =======
    >>>>>>> updated
    ```
    
    Next, we will add the `main` function to `app.py`.
    
    ```python
    app.py
    <<<<<<< HEAD
    =======
    import streamlit as st
    from transcriber import conversation_transcription, convert_mp3_to_wav
    
    def main():
        st.title("Azure Meting Transcription")
        uploaded_file = st.file_uploader("Choose a file", type=["mp3"])
        if uploaded_file is not None:
            file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type,"FileSize":uploaded_file.size}
            st.write(file_details)
    
            # Convert the uploaded mp3 file to a wav file with a spinner to indicate progress
            wav_file_path = "temp.wav"
            with st.spinner('Converting audio file to wav format...'):
                convert_mp3_to_wav(uploaded_file, wav_file_path)
    
            with st.spinner('Processing audio file...'):
                transcriptions = conversation_transcription(wav_file_path)
            st.text_area("Transcription", "\n".join(transcriptions), height=200)
    
    if __name__ == "__main__":
        main()
    >>>>>>> updated
    ```
    
    Please apply these changes to your code.

--- transcriber.py
+++ transcriber.py
@@ -109,23 +109,5 @@ def conversation_transcription_from_microphone():
 
 import streamlit as st
 
-def main():
-    st.title("Azure Meting Transcription")
-    uploaded_file = st.file_uploader("Choose a file", type=["mp3"])
-    if uploaded_file is not None:
-        file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type,"FileSize":uploaded_file.size}
-        st.write(file_details)
-        
-        # Convert the uploaded mp3 file to a wav file with a spinner to indicate progress
-        wav_file_path = "temp.wav"
-        with st.spinner('Converting audio file to wav format...'):
-            convert_mp3_to_wav(uploaded_file, wav_file_path)
-        
-        with st.spinner('Processing audio file...'):
-            transcriptions = conversation_transcription(wav_file_path)
-        st.text_area("Transcription", "\n".join(transcriptions), height=200)
-
-if __name__ == "__main__":
-    main()
     
 from audio_handler import convert_mp3_to_wav
